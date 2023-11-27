8452de8935c81a4f18129c7c96d28930efc1e037
ituphi <jfiturbeo@gmail.com>
2023-11-27 15:44:57 -0500

    Random Walker v001

--- noise/Walker.js
+++ noise/Walker.js
@@ -1,12 +1,43 @@
 class Walker {
-  constructor(x, y) {
+  constructor(x, y, r) {
     this.pos = createVector(x, y);
+    this.r = r;
   }
-  update() {
+  step() {
+    let choice = floor(random(5));
+    let step = createVector();
+
+    switch (choice) {
+      case 0:
+        step = createVector(3 + this.r, 0);
+        break;
+      case 1:
+        step = createVector(-3 - this.r, 0);
+        break;
+      case 2:
+        step = createVector(0, 3 + this.r);
+        break;
+      case 3:
+        step = createVector(0, -3 - this.r);
+        break;
+      case 4:
+        createVector(0, -3 - this.r);
+        break;
+    }
+    this.pos.add(step);
+  }
+
+  altStep() {
     this.pos.x = this.pos.x + random(-1, 1);
     this.pos.y = this.pos.y + random(-1, 1);
   }
+
   show() {
-    point(this.pos.x, this.pos.y);
+    colorMode(HSB);
+    stroke(0.5);
+    let hue = map(sin(radians(frameCount)), 0, 1, 0, 255);
+    fill(hue, 100, 50);
+    let scalarScale = sin(radians(frameCount));
+    circle(this.pos.x, this.pos.y, this.r * scalarScale);
   }
 }
--- noise/sketch.js
+++ noise/sketch.js
@@ -1,18 +1,11 @@
-let xoff = 0;
-let xoff2 = 10000;
+let walker;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 900);
+  walker = new Walker(width / 2, height / 2, 25);
 }
-
 function draw() {
-  background(220);
-  xoff += 0.01;
-  xoff2 += 0.02;
-  let x = map(noise(xoff), 0, 1, 0, width);
-  let y = map(noise(xoff2), 0, 1, 0, width);
-
-  console.log(xoff);
-  fill(color("red"));
-  ellipse(x, y, 20, 20);
+  // background(220);
+  walker.step();
+  walker.show();
 }
